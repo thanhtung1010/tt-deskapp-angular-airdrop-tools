bf58c7486bd0109ffbc218006d0228f370646d53
tung.trinh <tung.trinh@realestatedoc.co>
2025-03-04 15:59:42 +0700

    init electron

---
+++ main.js
@@ -0,0 +1,43 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+    },
+  });
+
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, 'dist/tt-deskapp-angular-airdrop-tools/browser/index.html'),
+      protocol: 'file:',
+      slashes: true,
+    })
+  );
+
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
