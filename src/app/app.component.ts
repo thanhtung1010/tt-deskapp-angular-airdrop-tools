3dae8b7d2a13956914ab737e1eb38b78abb0c248
tung.trinh <tung.trinh@realestatedoc.co>
2025-03-06 18:06:23 +0700

    update firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CreateDocumentModel, FirebaseCollectionService, FirebaseService } from 'common-service';
+import { Expose } from 'class-transformer';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'tt-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.sass',
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'tt-deskapp-angular-airdrop-tools';
+  name: string = '';
+  code: string = '';
+
+  constructor(
+    private fbService: FirebaseService,
+    private fbCollectionService: FirebaseCollectionService<ICommon>,
+  ) {}
+
+  ngOnInit(): void {
+    this.fbService.init().subscribe(resp => {
+      console.warn('init firebase success')
+    });
+    this.fbCollectionService.collection = 'TEST';
+  }
+
+  submit() {
+    const data = {
+      name: this.name,
+      code: this.code,
+    }
+    this.fbCollectionService.addNewDocument(CommonModel.fromJson(data)).subscribe(resp => {
+      console.log(resp);
+
+    })
+  }
+}
+
+export class CommonModel extends CreateDocumentModel {
+  @Expose()
+  name!: string;
+
+  @Expose()
+  code!: string;
+}
+
+export interface ICommon {
+  name: string;
+  code: string;
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,23 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { FirebaseModule } from 'common-service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    importProvidersFrom([
+      FirebaseModule.forRoot({
+        apiKey: "AIzaSyCpRMBZYgwrigZH1hn2o8C7wxYbDFioSdM",
+        authDomain: "genealogy-564ae.firebaseapp.com",
+        projectId: "genealogy-564ae",
+        storageBucket: "genealogy-564ae.firebasestorage.app",
+        messagingSenderId: "698023068308",
+        appId: "1:698023068308:web:f28096240049387f046a81",
+        measurementId: "G-16WV9QSS7Y"
+      })
+    ]),
+  ],
 };
